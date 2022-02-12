e6472a7c75d7c03430b51f15f6751ccb9005ed9a
Daichi Suzuki <ds.81ueman@gmail.com>
2022-02-12 18:44:00 +0900

    fixed imem for ras

--- vliw/imem_ram.sv
+++ vliw/imem_ram.sv
@@ -5,10 +5,10 @@ module imem_ram(
         input  logic [13:0] pc,
         input  logic [13:0] npc,
         input  logic npc_enn,
-        input  logic [29:0] daddr,
-        input  logic [31:0] dec_op2,
-        input  logic dec_mwe,
-        input  logic n_stall,
+        input  logic [29:0] daddr3,
+        input  logic [31:0] dec_op32,
+        input  logic dec_mwe3,
+        input  logic stall,
         input  logic flush,
         output logic [127:0] inst,
         output logic [13:0] if_pc
@@ -23,6 +23,8 @@ module imem_ram(
         //$readmemh("loader.mem", mem, 16346, 16383); //check pc
     end
     //assign inst = rst ? 0 : mem[pc[13:2]];
+    logic [1:0] offset;
+    assign offset = daddr3[1:0];
     always_ff @( posedge clk ) begin 
         if(rst ) begin
             inst <= 0;
@@ -30,7 +32,7 @@ module imem_ram(
         /*end else if (flush)begin
             inst <= 0;*/
         end else begin
-            if(n_stall) begin
+            if(~stall) begin
                 if(npc_enn) begin
                     inst <= mem[npc[13:0]];
                     if_pc <= npc;
@@ -39,8 +41,12 @@ module imem_ram(
                     if_pc <= pc;
                 end
                 // boot loaderは後で考える。
-                if (&daddr[29:25] & dec_mwe) begin
-                    mem[daddr[13:0]] <= dec_op2;
+                if (daddr3[29:25] == 5'b11110 & dec_mwe3) begin
+                    if(offset == 2'b00)mem[daddr3[13:0]][31:0] <= dec_op32;
+                    if(offset == 2'b01)mem[daddr3[13:0]][63:32] <= dec_op32;
+                    if(offset == 2'b10)mem[daddr3[13:0]][95:64] <= dec_op32;
+                    if(offset == 2'b11)mem[daddr3[13:0]][127:96] <= dec_op32;
+                    
                 end
             end
         end
