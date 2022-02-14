413cf4af4b14e35da692612032df5dc900a03fcf
Daichi Suzuki <ds.81ueman@gmail.com>
2022-02-15 05:16:10 +0900

    fixed jumpr/callr

--- vliw/branchjump.sv
+++ vliw/branchjump.sv
@@ -1,6 +1,7 @@
 module branchjump(
     input  logic beq, bne, blt,bge,
     input  logic dec_jumpr,
+    input  logic stall,
     output logic [31:0] op11,op12,
     output logic npc_enn,
     output logic flush
@@ -10,7 +11,7 @@ module branchjump(
     always_comb begin 
         eq = op11 == op12;
         lt = $signed(op11) < $signed(op12);
-        npc_enn = beq && eq || bne && ~eq || blt && lt || bge && ~lt || dec_jumpr;
+        npc_enn = (beq && eq || bne && ~eq || blt && lt || bge && ~lt || dec_jumpr) && ~stall;
         flush = npc_enn;
     end
 endmodule
